63cb59f3f6dc59ed01a0506c179cb0956a9eae5b
mohammadkarrabi <78946883+mohammadkarrabi@users.noreply.github.com>
2023-11-25 12:34:06 +0330

    Update get_images.sh

--- get_images.sh
+++ get_images.sh
@@ -1,10 +1,1 @@
-#!/usr/bin/expect -f
-
-while true; do
-    # Run the scp command here
-    sshpass -p 'oNVKMXlvmj1' scp -r ubuntu@188.213.199.150:/home/ubuntu/crawl_and_match/best-images/* ./images/candidates
-    sshpass -p 'oNVKMXlvmj1' scp -r ubuntu@188.213.199.150:/home/ubuntu/crawl_and_match/best-images/* ./images/candidates
-    sshpass -p 'oNVKMXlvmj1' scp -r ubuntu@188.213.199.150:/home/ubuntu/crawl_and_match/best-images/* ./images/candidates
-    # Sleep for 30 seconds
-    sleep 5
-done
+#nothing!
